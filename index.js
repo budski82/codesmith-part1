44e0bb2ce109536d9aedb841ce818c9d3d3c4d67
William Rutkowski <budski82@hotmail.com>
2020-09-10 17:46:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const myGrade = 75;
+
+    if(myGrade <= 70)
+    {
+        console.log(myGrade + " is failing.");
+    }
+    else if(myGrade < 80)
+    {
+        console.log(myGrade + " is passing.");
+    }
+    else
+    {
+        console.log(myGrade + " is great!");
+    }
+/*
+    if(myGrade === 100)
+    {
+        console.log(" my score is perect!!")
+    } */
